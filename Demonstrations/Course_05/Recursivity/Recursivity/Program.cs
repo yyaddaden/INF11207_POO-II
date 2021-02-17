4790e194e390cdbce2dad4586a4c7820620a7f73
Yacine YADDADEN <yacine.yaddaden@gmail.com>
2021-02-17 00:05:42 -0500

    Update: add exercices and demonstrations about recursivity :sparkles:.

---
+++ Demonstrations/Course_05/Recursivity/Recursivity/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursivity
+{
+    class Program
+    {
+        /* a. Récursivité simple */
+
+        /* Exemple I */
+        static int puissance(int x, int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+            {
+                return x * puissance(x, n - 1);
+            }
+        }
+
+        /* Exemple II */
+        static int somme(int n)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return n + somme(n - 1);
+        }
+
+        /* b. Récursivité multiple */
+
+        /* Exemple I */
+        static int combinaison(int n, int p)
+        {
+            if (n == 1 || p == n)
+                return 1;
+            else
+            {
+                return combinaison(n, p - 1) + combinaison(n - 1, p - 1);
+            }
+
+        }
+
+        /* Exemple II */
+        static int fibonacci(int n)
+        {
+
+            if (n < 2)
+                return n;
+            else
+            {
+                return fibonacci(n - 1) + fibonacci(n - 2);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            /* a.I */
+            int x = 5, n = 3;
+            Console.WriteLine($"Le résultat de {x}^{n} = {puissance(x, n)}");
+
+            /* a.II */
+            Console.WriteLine(somme(5));
+
+            /* b.I */
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{combinaison(j, i)} ");
+                }
+                Console.Write("\n");
+            }
+
+            /* b.II */
+            Console.Write("0 1 ");
+            for (int i = 2; i < 8; i++)
+            {
+                Console.Write($"{fibonacci(i)} ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercices/Course_05/Recursivity/Recursivity/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursivity
+{
+    class Program
+    {
+        /* Exercice I */
+        static int PGCD(int n, int m)
+        {
+            if (n == m)
+                return n;
+            else if (n > m)
+                return PGCD(n - m, m);
+            else
+                return PGCD(n, m - n);
+        }
+
+        /* Exercice II */
+        static void Dec2Bin(int n)
+        {
+            if (n == 0)
+                Console.Write("0");
+            else if (n == 1)
+                Console.Write("1");
+            else
+            {
+                Dec2Bin(n / 2);
+                Console.Write(n % 2);
+            }
+        }
+
+        /* Exercice III */
+        static int MaxTab(int[] tab, int n)
+        {
+            int m = -1;
+
+            if (n == 1)
+                return tab[0];
+            else
+                m = MaxTab(tab, n - 1);
+
+            if (m > tab[n - 1])
+                return m;
+            else
+                return tab[n - 1];
+        }
+
+        /* Exercice IV */
+        static bool Palin(string mot)
+        {
+            if (mot.Length == 1)
+                return true;
+            else if (mot[0] == mot[mot.Length - 1])
+                return Palin(mot.Substring(1, mot.Length - 2));
+            else
+                return false;
+        }
+
+        /* Exercice V */
+        static int Pascal(int lin, int col)
+        {
+            if (col == 0 || col == lin)
+                return 1;
+            else
+                return Pascal(lin - 1, col - 1) + Pascal(lin - 1, col);
+        }
+
+        static void Main(string[] args)
+        {
+            /* Exercice I */
+            Console.WriteLine(PGCD(12, 36));
+
+            /* Exercice II */
+            Dec2Bin(9);
+
+            /* Exercice III */
+            int[] tab = new int[] { 4, 8, 10, 78, 6, 2 };
+            Console.WriteLine(MaxTab(tab, tab.Length));
+
+            /* Exercice IV */
+            Console.WriteLine(Palin("ressasser"));
+
+            /* Exercice V */
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write($"{Pascal(i, j)} ");
+                }
+                Console.Write("1\n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
