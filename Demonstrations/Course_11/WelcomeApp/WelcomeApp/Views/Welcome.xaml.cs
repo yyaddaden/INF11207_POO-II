057531ce87dfe2912af7bee4ad7f35b11ad6c607
Yacine YADDADEN <yacine.yaddaden@gmail.com>
2021-03-24 21:27:36 -0400

    :sparkles: add source code WelcomeApp.

---
+++ Demonstrations/Course_11/WelcomeApp/WelcomeApp/Views/Welcome.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WelcomeApp.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Welcome : Window
+    {
+        public Welcome()
+        {
+            InitializeComponent();
+        }
+
+        private void apply_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBoxItem comboBoxItem = (ComboBoxItem)personJob.SelectedValue;
+            if (comboBoxItem == null || personName.Text == "")
+                MessageBox.Show("Veuillez remplir tous les champs !", "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+            else
+                welcomeWords.Content = $"Bienvenue {personName.Text} ({comboBoxItem.Content})";
+        }   
+    }
+}
