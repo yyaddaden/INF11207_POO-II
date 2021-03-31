2b19367b4f23345514cd17b4e2f4ea1c9b8d9cdc
Yacine YADDADEN <yacine.yaddaden@gmail.com>
2021-03-31 00:40:07 -0400

    :sparkles: add source code WelcomeAppWithMVVM.

---
+++ Demonstrations/Course_12/WelcomeAppWithMVVM/WelcomeAppWithMVVM/Models/Greeting.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeAppWithMVVM.Models
+{
+    class Greeting : INotifyPropertyChanged
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    SetIsValid();
+                }
+            }
+        }
+
+        private string _job;
+
+        public string Job
+        {
+            get { return _job; }
+            set
+            {
+                if (_job != value)
+                {
+                    _job = value;
+                    SetIsValid();
+                }
+            }
+        }
+
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                if (_message != value)
+                {
+                    _message = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private bool _isValid;
+
+        public bool IsValid
+        {
+            get { return _isValid; }
+        }
+
+        private void SetIsValid()
+        {
+            _isValid = !string.IsNullOrEmpty(Name) && !string.IsNullOrEmpty(Job);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Demonstrations/Course_12/WelcomeAppWithMVVM/WelcomeAppWithMVVM/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WelcomeAppWithMVVM.ViewModels
+{
+    class WelcomeViewModel
+    {
+        public Models.Greeting Greeting { get; set; }
+        public ObservableCollection<string> Jobs { get; set; }
+
+        public ICommand GreetingCommand { get; private set; }
+
+        public WelcomeViewModel()
+        {
+            Greeting = new Models.Greeting();
+            Jobs = new ObservableCollection<string>()
+            {
+                "Étudiant",
+                "Ingénieur",
+                "Professeur",
+                "Directeur"
+            };
+
+            GreetingCommand = new RelayCommand(
+                o => Greeting.IsValid,
+                o => DisplayMessage()
+            );
+        }
+
+        private void DisplayMessage()
+        {
+            Greeting.Message = $"Bienvenue {Greeting.Name} ({Greeting.Job})";
+        }
+    }
+}
